46dd1cc3717553e6012a0532340bfbe945dde316
James-Lee-code <acc22331@gmail.com>
2026-01-03 22:41:49 +0800

    fix netlify asset path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/',
+    buildAssetsDir: '/_nuxt/'
+  },
+
   nitro: {
     preset: 'netlify',
     prerender: { crawlLinks: false } 
